fd1dfe80d6b1b42fa9e24c35ae7d7f6171a968b1
Yannick Mahe <mahe.yannick@gmail.com>
2013-03-05 22:18:58 +0100

    started layout

--- src/YannickMahe/SelfHostedVideosBundle/Controller/DefaultController.php
+++ src/YannickMahe/SelfHostedVideosBundle/Controller/DefaultController.php
@@ -3,11 +3,25 @@
 namespace YannickMahe\SelfHostedVideosBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('YannickMaheSelfHostedVideosBundle:Default:index.html.twig');
+    }
+
+    //List & search engine results
+    public function listAction(Request $request)
+    {
+        $terms = $request->query->get('q');
+    	return $this->render('YannickMaheSelfHostedVideosBundle:Default:list.html.twig',array('terms' => $terms));
+    }
+
+    //Video page
+    public function videoAction($videoId)
+    {
+    	return $this->render('YannickMaheSelfHostedVideosBundle:Default:video.html.twig');
     }
 }
